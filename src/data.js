8de10b5bf3ed8fd9d5acb95ad8f56944cf987c04
OlgaMakeikina <makeikina2202@gmail.com>
2023-10-08 14:56:13 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,66 @@
+import eggsImage from './eggs.avif';
+import cerealImage from './cereal.avif';
+import granolaImage from './granola.avif';
+import pancakesImage from './pancakes.avif';
+import sandwichImage from './sandwich.avif';
+import yogurtImage from './yogurt.avif';
+import toastImage from './toast.avif';
+
+export const data = [
+
+    {
+        id: 1,
+        day: "Monday",
+        name: "Fried eggs",
+        image: eggsImage,
+        description: "1"
+    },
+
+    {
+        id: 2,
+        day: "Tuesday",
+        name: "Porridge",
+        image: cerealImage,
+        description: "1"
+    },
+
+    {
+        id: 3,
+        day: "Wednesday",
+        name: "Granola with fruit",
+        image: granolaImage,
+        description: "1"
+    },
+
+    {
+        id: 4,
+        day: "Thursday",
+        name: "Pancakes",
+        image: pancakesImage,
+        description: "1"
+    },
+
+    {
+        id: 5,
+        day: "Friday",
+        name: "Sandwich",
+        image: sandwichImage,
+        description: "1"
+    },
+
+    {
+        id: 6,
+        day: "Saturday",
+        name: "Yogurt with chia",
+        image: yogurtImage,
+        description: "1"
+    },
+
+    {
+        id: 7,
+        day: "Sunday",
+        name: "Toast with avocado",
+        image: toastImage,
+        description: "1"
+    }
+]
