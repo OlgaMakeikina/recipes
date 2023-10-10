25afdea63331185004bb0d58889448e2272c0761
OlgaMakeikina <makeikina2202@gmail.com>
2023-10-10 10:50:33 -0300

    the code was finished

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 function App() {
 const [weekMenu, setWeekMenu] = useState(0);
-const {id, day, name, image, description} = data[weekMenu];
 const [menu, setMenu] = useState(data);
+const [showMore, setShowMore] = useState(false);
 const removeRecipe = (id) => {
   let newArray = menu.filter(recipe => recipe.id !== id)
   setMenu(newArray);
@@ -16,7 +16,7 @@ const previousRecipe = () => {
   setWeekMenu ((weekMenu => {
     weekMenu --;
     if (weekMenu<0) {
-      return data.length-1;
+      return menu.length-1;
     }
     return weekMenu
   }))
@@ -25,7 +25,7 @@ const previousRecipe = () => {
 const nextRecipe = () => {
   setWeekMenu ((weekMenu => {
     weekMenu ++;
-    if (weekMenu > data.length-1) {
+    if (weekMenu > menu.length-1) {
       weekMenu = 0;
     }
     return weekMenu
@@ -35,29 +35,32 @@ const nextRecipe = () => {
 
 
 return (
+  menu.length > 0 &&
   <div className='container'>
         <h1>List of {menu.length} ideas for breakfast</h1>
+
   <div> 
-     <button className='btn' onClick={previousRecipe}>Previous</button>
+<h2>{menu[weekMenu].id} - {menu[weekMenu].day} - {menu[weekMenu].name}</h2>
+</div>
+<div>
+<button className='removeBtn' onClick={() => removeRecipe(menu[weekMenu].id)}>Remove recipe</button>
+<button className='removeBtn' onClick={() => setMenu([])}>Remove all</button>
+</div>
+<div className='scroll'>
+<div> 
+     <button className='btn' onClick={previousRecipe}>&laquo;</button>
   </div>
-  
-  <div> 
-<h2>{day}</h2>
-</div>
-  <div> 
-<h2>{id} - {name}</h2>
+<div> 
+<img src={menu[weekMenu].image} alt="recipe" width="300px" height="400px"/>
 </div>
 <div> 
-<img src={image} alt="recipe" width="300px" height="400px"/>
-</div>
-<div> 
-<p>{description}</p>
-</div>
-<div> 
-     <button className='btn' onClick={nextRecipe}>Next</button>
+     <button className='btn' onClick={nextRecipe}>&raquo;</button>
   </div>
-<button onClick={() => removeRecipe(id)}>Remove</button>
-<button onClick={() => setMenu([])}>Remove all</button>
+  </div>
+<div> 
+<p>{showMore? menu[weekMenu].description : menu[weekMenu].description.substring(0,70) + "..."}
+<button className="showBtn" onClick = {() => setShowMore(!showMore)}>{showMore? "Show less" : "Show more"}</button></p>
+</div>
 
 </div>
 
--- src/data.js
+++ src/data.js
@@ -13,7 +13,8 @@ export const data = [
         day: "Monday",
         name: "Fried eggs",
         image: eggsImage,
-        description: "1"
+        description: "In a small nonstick over medium heat, melt butter (or heat oil). Crack egg into pan. Cook 3 minutes, or until white is completely set. Remove from pan and season with salt and pepper.",
+        showMore: false
     },
 
     {
@@ -21,7 +22,8 @@ export const data = [
         day: "Tuesday",
         name: "Porridge",
         image: cerealImage,
-        description: "1"
+        description: "Put 50g porridge oats in a saucepan, pour in 350ml milk or water and sprinkle in a pinch of salt. Bring to the boil and simmer for 4-5 minutes, stirring from time to time and watching carefully that it doesn’t stick to the bottom of the pan.",
+        showMore: false
     },
 
     {
@@ -29,7 +31,8 @@ export const data = [
         day: "Wednesday",
         name: "Granola with fruit",
         image: granolaImage,
-        description: "1"
+        description: "Heat oven to 160C/fan 140C/gas 3. Heat the oil, honey and maple syrup together in a pan. Mix the oats, almonds, pine nuts, puffed rice and sesame seeds in a large mixing bowl. Pour over the honey mix, stir well to coat, then tip onto a large baking tray. Bake for 15 mins until everything is golden and crisp. Take the tray from the oven, leave to cool, then break up any big clumps. Mix together with the dried fruit and coconut shavings. Serve with Greek yogurt and fresh raspberries. The rest of the granola can be stored in a sealed jar and enjoyed over the next 2 weeks.",
+        showMore: false
     },
 
     {
@@ -37,7 +40,8 @@ export const data = [
         day: "Thursday",
         name: "Pancakes",
         image: pancakesImage,
-        description: "1"
+        description: "Combine flour, sugar, baking powder, and salt in a large bowl. Make a well in the center, and pour in milk, oil, and egg. Mix until smooth. Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop batter onto the griddle, using approximately 1/4 cup for each pancake; cook until bubbles form and the edges are dry, 1 to 2 minutes. Flip and cook until browned on the other side. Repeat with remaining batter.",
+        showMore: false
     },
 
     {
@@ -45,7 +49,8 @@ export const data = [
         day: "Friday",
         name: "Sandwich",
         image: sandwichImage,
-        description: "1"
+        description: "Heat grill to High and cook the bacon, turning halfway through, until crisp. Toast the bread and spread one slice with the mayo. Layer on the egg, tomato and bacon, then top with another slice of bread. Add the chicken or turkey, bacon, then lettuce. Finish with the final slice of bread and cut into quarters. Secure with cocktail sticks and serve with crisps, if you like.",
+        showMore: false
     },
 
     {
@@ -53,7 +58,8 @@ export const data = [
         day: "Saturday",
         name: "Yogurt with chia",
         image: yogurtImage,
-        description: "1"
+        description: "Combine the chia seeds, milk and 2 tsp maple syrup in a jar or bowl. Cover and chill overnight or for at least 8 hrs. When ready to serve, top with fruit and a drizzle of maple syrup.",
+        showMore: false
     },
 
     {
@@ -61,6 +67,7 @@ export const data = [
         day: "Sunday",
         name: "Toast with avocado",
         image: toastImage,
-        description: "1"
+        description: "Cut the avocado in half and carefully remove its stone, then scoop out the flesh into a bowl. Squeeze in the lemon juice then mash with a fork to your desired texture. Season to taste with sea salt, black pepper and chilli flakes. Toast your bread, drizzle over the oil then pile the avocado on top.",
+        showMore: false
     }
 ]
